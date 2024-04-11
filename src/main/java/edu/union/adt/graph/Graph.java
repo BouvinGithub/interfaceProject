f06487db29df3bbacf40c1f0f907d5c79261b6a4
Grant Bouvin <bouving@union.edu>
2024-04-10 21:02:44 -0400

    My final completion of Graph and Vertex that work

--- src/main/java/edu/union/adt/graph/Graph.java
+++ src/main/java/edu/union/adt/graph/Graph.java
@@ -1,7 +1,6 @@
 package edu.union.adt.graph;
-import java.util.ArrayList;
+import java.util.*;
 import java.lang.RuntimeException;
-import java.util.Stack;
 import java.lang.StringBuilder;
 
 /**
@@ -9,68 +8,72 @@ import java.lang.StringBuilder;
  * (parameterized) type V (vertices).  The edges are directed.  An
  * undirected edge between u and v can be simulated by two edges: (u,
  * v) and (v, u).return "";
- *
+ * <p>
  * The API is based on one from
  *     <a href="http://introcs.cs.princeton.edu/java/home/">...</a>
- *
+ * <p>
  * Some method names have been changed, and the Graph type is
  * parameterized with a vertex type V instead of assuming String
  * vertices.
+ *
+ * I received help from Jason at the CS Help Desk on this project
  *
  * @author Grant Bouvin
  * @version 4/6/2024
  */
 public class Graph<V> {
 
-    private ArrayList<Vertex<V>> vertexList;
+    private HashMap<V, Vertex<V>> vertexList;
+    private int capacity;
     private int vertices;
     private int edges;
 
-   /**
-    * Create an empty graph.
-    */
+    /**
+     * Create an empty graph.
+     */
     public Graph() {
-      final int DEFAULT_CAPACITY = 10;
-      vertexList = new ArrayList<>(DEFAULT_CAPACITY);
-      vertices = 0;
-      edges = 0;
+        final int DEFAULT_CAPACITY = 10;
+        vertexList = new HashMap<>(DEFAULT_CAPACITY);
+        capacity = 10;
+        vertices = 0;
+        edges = 0;
     }
 
-   /**
-    * @return the number of vertices in the graph.
-    */
+    /**
+     * @return the number of vertices in the graph.
+     */
     public int numVertices() {
         return vertices;
     }
 
-   /**
-    * @return the number of edges in the graph.
-    */
+    /**
+     * @return the number of edges in the graph.
+     */
     public int numEdges() {
         return edges;
     }
 
-   /**
-    * Gets the number of vertices connected by edges from a given
-    * vertex.  If the given vertex is not in the graph, throws a
-    * RuntimeException.
-    *
-    * @param vertex the vertex whose degree we want.
-    * @return the degree of vertex 'vertex'
-    */
+    /**
+     * Gets the number of vertices connected by edges from a given
+     * vertex.  If the given vertex is not in the graph, throws a
+     * RuntimeException.
+     *
+     * @param vertex the vertex whose degree we want.
+     * @return the degree of vertex 'vertex'
+     */
     public int degree(V vertex) throws RuntimeException {
         return getVertex(vertex).getDegree();
     }
 
-   /**
-    * Adds a directed edge between two vertices.  If there is already an edge
-    * between the given vertices, does nothing.  If either (or both)
-    * of the given vertices does not exist, it is added to the
-    * graph before the edge is created between them.
-    *
-    * @param from the source vertex for the added edge
-    * @param to the destination vertex for the added edge
-    */
+    /**
+     * Adds a directed edge between two vertices.  If there is already an edge
+     * between the given vertices, does nothing.  If either (or both)
+     * of the given vertices does not exist, it is added to the
+     * graph before the edge is created between them.
+     *
+     * @param from the source vertex for the added edge
+     * @param to the destination vertex for the added edge
+     */
     public void addEdge(V from, V to) {
         if (!this.contains(from)) {
             addVertex(from);
@@ -80,211 +83,197 @@ public class Graph<V> {
             addVertex(to);
             vertices++;
         }
-        getVertex(from).addEdge(to);
-        edges++;
+        Vertex<V> myVertexFrom = getVertex(from);
+        if (!myVertexFrom.hasEdge(to)) {
+            myVertexFrom.addEdge(to);
+            edges++;
+        }
     }
 
-   /**
-    * Adds a vertex to the graph.  If the vertex already exists in
-    * the graph, does nothing.  If the vertex does not exist, it is
-    * added to the graph, with no edges connected to it.
-    *
-    * @param vertex the vertex to add
-    */
+    /**
+     * Adds a vertex to the graph.  If the vertex already exists in
+     * the graph, does nothing.  If the vertex does not exist, it is
+     * added to the graph, with no edges connected to it.
+     *
+     * @param vertex the vertex to add
+     */
     public void addVertex(V vertex) {
         if (!this.contains(vertex)) {
-            vertexList.add(new Vertex<V>(vertex));
+            ensureCapacity(numVertices()+1);
+            vertexList.put(vertex, new Vertex<>(vertex));
             vertices++;
         }
     }
 
-   /**
-    * @return an iterable collection for the set of vertices of
-    * the graph.
-    */
+    /**
+     * @return an iterable collection for the set of vertices of
+     * the graph.
+     */
     public Iterable<V> getVertices() {
-        Stack<V> toReturn = new Stack<>();
-        if (!isEmpty()) {
-          for (Vertex<V> vertex : vertexList) {
-            toReturn.push(vertex.getSource());
-          }
-        }
-        return toReturn;
-    }
-
-   /**
-    * Gets the vertices adjacent to a given vertex.  A vertex y is
-    * "adjacent to" vertex x if there is an edge (x, y) in the graph.
-    * Because edges are directed, if (x, y) is an edge but (y, x) is
-    * not an edge, we would say that y is adjacent to x but that x is
-    * NOT adjacent to y.
-    *
-    * @param from the source vertex
-    * @return an iterable collection for the set of vertices that are
-    * the destinations of edges for which 'from' is the source
-    * vertex.  If 'from' is not a vertex in the graph, returns an
-    * empty iterator.
-    */
-    public Iterable<V> adjacentTo(V from) {
-      Stack<V> toReturn = new Stack<>();
-      if (!isEmpty()) {
-        for (Vertex<V> vertex : vertexList) {
-          if (vertex.isAdjacent(from)) {
-            toReturn.push(vertex.getSource());
-          }
-        }
-      }
-      return toReturn;
-    }
-
-   /**
-    * Tells whether a vertex is in the graph.
-    *
-    * @param vertex a vertex
-    * @return true iff 'vertex' is a vertex in the graph.
-    */
-    public boolean contains(V vertex) {
-      if (!isEmpty()) {
-        for(Vertex<V> vert : vertexList) {
-            if (vert.fromSource(vertex)) {
-                return true;
-            }
-        }
-      }
-      return false;
-    }
-
-   /**
-    * Tells whether an edge exists in the graph.
-    *
-    * @param from the source vertex
-    * @param to the destination vertex
-    *
-    * @return true iff there is an edge from the source vertex to the
-    * destination vertex in the graph.  If either of the given
-    * vertices are not vertices in the graph, then there is no edge
-    * between them.
-    */
-    public boolean hasEdge(V from, V to) {
-      if (!isEmpty()) {
-        if (this.contains(from) && this.contains(to)) {
-          return getVertex(from).hasEdge(to);
-        }
-      }
-      return false;
-    }
-
-   /**
-    * Gives a string representation of the graph.  The representation
-    * is a series of lines, one for each vertex in the graph.  On
-    * each line, the vertex is shown followed by ": " and then
-    * followed by a list of the vertices adjacent to that vertex.  In
-    * this list of vertices, the vertices are separated by ", ".  For
-    * example, for a graph with String vertices "A", "B", and "C", we
-    * might have the following string representation:
-    *
-    * <PRE>
-    * A: A, B
-    * B:
-    * C: A, B
-    * </PRE>
-    *
-    * This representation would indicate that the following edges are
-    * in the graph: (A, A), (A, B), (C, A), (C, B) and that B has no
-    * adjacent vertices.
-    *
-    * Note: there are no extraneous spaces in the output.  So, if we
-    * replace each space with '*', the above representation would be:
-    *
-    * <PRE>
-    * A:*A,*B
-    * B:
-    * C:*A,*B
-    * </PRE>
-    *
-    * @return the string representation of the graph
-    */
-    public String toString() {
-      if (isEmpty()) {
-        return "";
-      }
-      else {
-        StringBuilder toReturn = new StringBuilder();
-        for(Vertex<V> vertex : vertexList) {
-          toReturn.append(vertex.toString());
-          toReturn.append("\n");
-        }
-        toReturn.deleteCharAt(toReturn.length()-1);
-        return toReturn.toString();
-      }
-
+        return (Iterable<V>) vertexList.keySet();
     }
 
     /**
+     * Gets the vertices adjacent to a given vertex.  A vertex y is
+     * "adjacent to" vertex x if there is an edge (x, y) in the graph.
+     * Because edges are directed, if (x, y) is an edge but (y, x) is
+     * not an edge, we would say that y is adjacent to x but that x is
+     * NOT adjacent to y.
      *
-     *
+     * @param from the source vertex
+     * @return an iterable collection for the set of vertices that are
+     * the destinations of edges for which 'from' is the source
+     * vertex.  If 'from' is not a vertex in the graph, returns an
+     * empty iterator.
      */
-    public boolean equals(Object anotherObject){
-      if (!(anotherObject instanceof Graph)) {
-        return false;
-      }
-      else {
-        Graph anotherGraph = (Graph) anotherObject;
-        if (this.numVertices() != anotherGraph.numVertices()) {
-          return false;
-        }
-        else if (this.numEdges() != anotherGraph.numEdges()) {
-          return false;
+    public Iterable<V> adjacentTo(V from) {
+        if(contains(from)) {
+            return getVertex(from).getEdges();
         }
         else {
-          
+            return (Iterable<V>) Collections.emptyIterator();
         }
-      }
-
-    }
-    //   if (this.numVertices() != anotherGraph.numVertices()) {
-    //     return false;
-    //   }
-    //   else if (this.numEdges() != anotherGraph.numEdges()) {
-    //     return false;
-    //   }
-    //   else {
-    //     Iterable<V> container = anotherGraph.getVertices();
-    //     for (V vertex : container) {
-    //       if (!this.contains(vertex)) {
-    //         return false;
-    //       }
-    //       else {
-    //         Iterable<V> edges = vertex.getEdges();
-    //       }
-    //     }
-    //   }
-    // }
-
-   /**
-    * gets the Vertex object that this graph contains of source vert
-    *
-    * @param vert the source of the Vertex object
-    * @return the Vertex object, null if there is no Vertex object
-    * of source vert
-    */
-    private Vertex<V> getVertex(V vert) {
-      if (!isEmpty()) {
-          for(Vertex<V> vertex : vertexList) {
-              if (vertex.fromSource(vert)) {
-                  return vertex;
-              }
-          }
-      }
-      return null;
     }
 
     /**
-     * @return true if this graph has no verticies, false otherwise
+     * Tells whether a vertex is in the graph.
+     *
+     * @param vertex a vertex
+     * @return true iff 'vertex' is a vertex in the graph.
      */
-     private boolean isEmpty() {
-       return numVertices()==0;
-     }
+    public boolean contains(V vertex) {
+        if (!isEmpty()) {
+            return vertexList.containsKey(vertex);
+        }
+        return false;
+    }
 
+    /**
+     * Tells whether an edge exists in the graph.
+     *
+     * @param from the source vertex
+     * @param to the destination vertex
+     *
+     * @return true iff there is an edge from the source vertex to the
+     * destination vertex in the graph.  If either of the given
+     * vertices are not vertices in the graph, then there is no edge
+     * between them.
+     */
+    public boolean hasEdge(V from, V to) {
+        if (!isEmpty()) {
+            if (this.contains(from) && this.contains(to)) {
+                return getVertex(from).hasEdge(to);
+            }
+        }
+        return false;
+    }
 
+    /**
+     * Gives a string representation of the graph.  The representation
+     * is a series of lines, one for each vertex in the graph.  On
+     * each line, the vertex is shown followed by ": " and then
+     * followed by a list of the vertices adjacent to that vertex.  In
+     * this list of vertices, the vertices are separated by ", ".  For
+     * example, for a graph with String vertices "A", "B", and "C", we
+     * might have the following string representation:
+     *
+     * <PRE>
+     * A: A, B
+     * B:
+     * C: A, B
+     * </PRE>
+     *
+     * This representation would indicate that the following edges are
+     * in the graph: (A, A), (A, B), (C, A), (C, B) and that B has no
+     * adjacent vertices.
+     * <p>
+     * Note: there are no extraneous spaces in the output.  So, if we
+     * replace each space with '*', the above representation would be:
+     *
+     * <PRE>
+     * A:*A,*B
+     * B:
+     * C:*A,*B
+     * </PRE>
+     *
+     * @return the string representation of the graph
+     */
+    public String toString() {
+        if (isEmpty()) {
+            return "";
+        }
+        else {
+            StringBuilder toReturn = new StringBuilder();
+            for(Vertex<V> vertex : vertexList.values()) {
+                toReturn.append(vertex.toString());
+                toReturn.append("\n");
+            }
+            toReturn.deleteCharAt(toReturn.length()-1);
+            return toReturn.toString();
+        }
+
+    }
+
+    /**
+     * compares this Graph with an Object
+     * @return true if the graph has the same edges and vertices, false otherwise
+     */
+    public boolean equals(Object anotherObject){
+        if (!(anotherObject instanceof Graph)) {
+            return false;
+        }
+        else {
+            Graph<?> anotherGraph = (Graph<?>) anotherObject;
+            if (this.numVertices() != anotherGraph.numVertices()) {
+                return false;
+            }
+            else if (this.numEdges() != anotherGraph.numEdges()) {
+                return false;
+            }
+            else {
+                try {
+                    Graph<V> myTypeGraph = (Graph<V>) anotherGraph;
+                    for(V vertex : this.getVertices()) {
+                        if (!myTypeGraph.contains(vertex)) {
+                            return false;
+                        }
+                    }
+                    return true;
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    /**
+     * gets the Vertex object that this graph contains of source vert
+     *
+     * @param vert the source of the Vertex object
+     * @return the Vertex object, null if there is no Vertex object
+     * of source vert
+     */
+    private Vertex<V> getVertex(V vert) {
+        return vertexList.get(vert);
+    }
+
+    /**
+     * @return true if this graph has no vertices, false otherwise
+     */
+    private boolean isEmpty() {
+        return numVertices()==0;
+    }
+
+    /**
+     * makes sure the graph has enough space for a newCapacity number of items
+     * @param newCapacity the graph will have at least newCapacity capacity
+     */
+    public void ensureCapacity(int newCapacity) {
+        if (newCapacity>capacity) {
+            int capacityModifier = 2;
+            HashMap<V, Vertex<V>> biggerMap = new HashMap<>(capacity*capacityModifier);
+            biggerMap.putAll(this.vertexList);
+            vertexList = biggerMap;
+        }
+    }
 }
--- src/main/java/edu/union/adt/graph/Vertex.java
+++ src/main/java/edu/union/adt/graph/Vertex.java
@@ -1,6 +1,8 @@
 package edu.union.adt.graph;
 import java.util.LinkedList;
+import java.util.Stack;
 import java.lang.StringBuilder;
+import java.util.Iterator;
 
 /**
  * A vertex on a graph. It contains any edges that
@@ -10,103 +12,123 @@ import java.lang.StringBuilder;
  * @version 4/6/2024
  */
 public class Vertex<V> {
-  private V source;
-  private int degree;
-  private LinkedList<V> edges;
+    private V source;
+    private int degree;
+    private LinkedList<V> edges;
 
- /**
-  * Create a vertex
-  * @param from the origin of the vertex
-  */
-  public Vertex(V from) {
-    source = from;
-    degree = 0;
-    edges = new LinkedList<>();
-  }
-
- /**
-  * @return a string representation of this object
-  */
-  public String toString() {
-    StringBuilder toReturn = new StringBuilder(source+":");
-    if (degree>0) {
-      toReturn.append(" ");
-      for (V vertex : edges) {
-        toReturn.append(vertex);
-        toReturn.append(", ");
-      }
-      toReturn.delete(toReturn.length()-2, toReturn.length());
+    /**
+     * Create a vertex
+     * @param from the origin of the vertex
+     */
+    public Vertex(V from) {
+        source = from;
+        degree = 0;
+        edges = new LinkedList<>();
     }
-    return toReturn.toString();
-  }
 
- /**
-  * @return true if this vertex is equal to anotherObject, false otherwwise
-  * (they are equal if they are both instances of the Vertex class and have the same source)
-  */
-  // public boolean equals(Vertex<V> anotherVertex) {
-  //   if(this.getSource() != anotherVertex.getSource()) {
-  //     return false;
-  //   }
-  //   else if (this.getDegree() != another.getDegree()) {
-  //     return false;
-  //   }
-  //   else {
-  //     for(V edge : edges)
-  //   }
-  //   }
-  // }
-
- /**
-  * adds an edge to this vertex from itself to vertex to,
-  * does nothing if there is already an edge there.
-  * @param to the end vertex of the edge to be added
-  */
-  public void addEdge(V to) {
-    if (!this.hasEdge(to)) {
-      edges.add(to);
-      degree++;
+    /**
+     * @return a string representation of this object
+     */
+    public String toString() {
+        StringBuilder toReturn = new StringBuilder(source+":");
+        if (degree>0) {
+            toReturn.append(" ");
+            for (V vertex : edges) {
+                toReturn.append(vertex);
+                toReturn.append(", ");
+            }
+            toReturn.delete(toReturn.length()-2, toReturn.length());
+        }
+        return toReturn.toString();
     }
-  }
 
- /**
-  * @return true if given source is the same as my source, false otherwise
-  */
-  public boolean fromSource(V source) {
-    return source == getSource();
-  }
+    /**
+     * @return true if this vertex is equal to anotherObject, false otherwise
+     * (they are equal if they are both instances of the Vertex class and have the same source)
+     */
+    public boolean equals(Object anotherObject) {
+        if (!(anotherObject instanceof Vertex)) {
+            return false;
+        }
+        else {
+            Vertex<?> anotherVertex = (Vertex<?>) anotherObject;
+            if (this.getSource() != anotherVertex.getSource()) {
+                return false;
+            } else if (this.getDegree() != anotherVertex.getDegree()) {
+                return false;
+            } else {
+                try {
+                    Vertex<V> myTypeVertex = (Vertex<V>) anotherVertex;
+                    for(V edge : this.getEdges()) {
+                        if (!myTypeVertex.hasEdge(edge)) {
+                            return false;
+                        }
+                    }
+                    return true;
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    /**
+     * adds an edge to this vertex from itself to vertex to,
+     * does nothing if there is already an edge there.
+     * @param to the end vertex of the edge to be added
+     */
+    public void addEdge(V to) {
+        if (!this.hasEdge(to)) {
+            edges.add(to);
+            degree++;
+        }
+    }
+
+    /**
+     * @return true if given source is the same as my source, false otherwise
+     */
+    public boolean fromSource(V source) {
+        return source == getSource();
+    }
 
 
- /**
-  * @return true if there is an edge starting at the source and ending
-  * at vertex to, false otherwise.
-  */
-  public boolean hasEdge(Object to) {
-    return edges.contains(to);
-  }
+    /**
+     * @return true if there is an edge starting at the source and ending
+     * at vertex to, false otherwise.
+     */
+    public boolean hasEdge(V to) {
+        return edges.contains(to);
+    }
 
- /**
-  * @return true if this vertex is adjacent to the given source vert,
-  * false otherwise.
-  */
-  public boolean isAdjacent(V source) {
-    return edges.contains(source);
-  }
+    /**
+     * @return true if this vertex is adjacent to the given source vert,
+     * false otherwise.
+     */
+    public boolean isAdjacent(V to) {
+        return edges.contains(to);
+    }
 
- /**
-  * @return the source of the vertex
-  */
-  public V getSource() {
-    return source;
-  }
+    /**
+     * @return the source of the vertex
+     */
+    public V getSource() {
+        return source;
+    }
 
- /**
-  * @return the degree of the vertex
-  */
-  public int getDegree() {
-    return degree;
-  }
+    /**
+     * @return the degree of the vertex
+     */
+    public int getDegree() {
+        return degree;
+    }
 
+    /**
+     * @return
+     */
+    public Iterable<V> getEdges() {
+        Stack myStack = new Stack<V>();
+        myStack.addAll(edges);
+        return myStack;
 
-
+    }
 }
